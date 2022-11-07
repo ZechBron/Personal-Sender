3ce09d6a69aa5341f2e95548d3c6769f1a5968ee
Zech Bron <39665654+ZechBron@users.noreply.github.com>
2022-11-07 19:07:03 +0700

    Create config.php

---
+++ config.php
@@ -0,0 +1,34 @@
+<?php
+
+
+/* ---[ SMTP CONFIG ]--- */
+$host = "";
+$port = 587;
+$username = "";
+$password = "";
+
+/* ---[ Sender Config ]--- */
+$SenderName = "";
+$Subject = "#" . rand();
+
+/* ---[ Other Config ]--- 
+beta version palang ito. saka ko na dagdagan inaatake ako ng katamaran eh
+*/
+$date = "";
+$time = "";
+
+$up = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+$randUp = $up[rand(0, 25)];
+
+$low = "abcdefghijklmnopqrstuvwxyz";
+$randLow = $low[rand(0, 25)];
+
+$num = rand(0, 9);
+
+
+//##randNum-##
+//##randLetterUp-##
+//##randLetterLow-##
+
+//for ($
+?>
